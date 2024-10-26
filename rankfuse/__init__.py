a1c38b6b5c53645ff6a64d2af600d4e637eba3a2
chu2bard <bxndnbdnd29@gmail.com>
2024-10-26 15:56:38 +0200

    add validator logic

--- rankfuse/__init__.py
+++ rankfuse/__init__.py
@@ -1,6 +1,7 @@
 """rankfuse - Reranking and result fusion for search/RAG."""
 
 __version__ = "0.1.0"
+# note: revisit later
 
 from rankfuse.rerank import CrossEncoderReranker, rerank
 from rankfuse.fusion import reciprocal_rank_fusion, weighted_score_fusion
