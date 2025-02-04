1a3a67c2f1ffd3a107d71ecc5a4a4a8b291edc8c
chu2bard <bxndnbdnd29@gmail.com>
2025-02-04 12:21:18 +0100

    implement retry

--- rankfuse/__init__.py
+++ rankfuse/__init__.py
@@ -1,17 +1,22 @@
 """rankfuse - Reranking and result fusion for search/RAG."""
 
-__version__ = "0.1.0"
-# note: revisit later
+__version__ = "0.2.0"
 
 from rankfuse.rerank import CrossEncoderReranker, rerank
-from rankfuse.fusion import reciprocal_rank_fusion, weighted_score_fusion
+from rankfuse.fusion import reciprocal_rank_fusion, weighted_score_fusion, interleave
 from rankfuse.types import SearchResult, RankedResult
+from rankfuse.utils import normalize_scores, deduplicate, filter_by_score, top_k
 
 __all__ = [
     "CrossEncoderReranker",
     "rerank",
     "reciprocal_rank_fusion",
     "weighted_score_fusion",
+    "interleave",
     "SearchResult",
     "RankedResult",
+    "normalize_scores",
+    "deduplicate",
+    "filter_by_score",
+    "top_k",
 ]
